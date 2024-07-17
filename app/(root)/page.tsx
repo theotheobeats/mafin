8a80c183b4282b0a4bbf15e674b35ce85c6cd7ae
Theo <asakvsa.idn@gmail.com>
2024-07-17 12:33:43 +0700

    dashboard optimization

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,26 +10,9 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const Page = () => {
-	const [userData, setUserData] = useState<any>(null);
-
-	useEffect(() => {
-		const fetchUserData = async () => {
-			try {
-				const data = await getProfile();
-
-				console.log(data);
-				setUserData(data.data);
-			} catch (error) {
-				console.error("Error fetching user data:", error);
-			}
-		};
-
-		fetchUserData();
-	}, []);
-
 	return (
 		<section className="w-full h-full p-8">
-			<Dashboard userData={userData} />
+			<Dashboard />
 		</section>
 	);
 };
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -5,80 +5,10 @@ import {
 } from "@/lib/actions/helper.action";
 import React, { useEffect, useState } from "react";
 
-import {
-	ChartConfig,
-	ChartContainer,
-	ChartTooltip,
-	ChartTooltipContent,
-} from "@/components/ui/chart";
-
-const chartConfig = {
-	desktop: {
-		label: "Desktop",
-		color: "hsl(var(--chart-1))",
-	},
-	mobile: {
-		label: "Mobile",
-		color: "hsl(var(--chart-2))",
-	},
-} satisfies ChartConfig;
-
-import {
-	Area,
-	AreaChart,
-	CartesianGrid,
-	Label,
-	Pie,
-	PieChart,
-	XAxis,
-} from "recharts";
-import { TrendingUp } from "lucide-react";
 import Link from "next/link";
 import TransactionItem from "./TransactionItem";
 import { Progress } from "./ui/progress";
-
-const pieChartData = [
-	{ browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-	{ browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-	{ browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
-	{ browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-	{ browser: "other", visitors: 190, fill: "var(--color-other)" },
-];
-
-const pieChartConfig = {
-	visitors: {
-		label: "Visitors",
-	},
-	chrome: {
-		label: "Chrome",
-		color: "hsl(var(--chart-1))",
-	},
-	safari: {
-		label: "Safari",
-		color: "hsl(var(--chart-2))",
-	},
-	firefox: {
-		label: "Firefox",
-		color: "hsl(var(--chart-3))",
-	},
-	edge: {
-		label: "Edge",
-		color: "hsl(var(--chart-4))",
-	},
-	other: {
-		label: "Other",
-		color: "hsl(var(--chart-5))",
-	},
-} satisfies ChartConfig;
-
-const chartData = [
-	{ month: "January", desktop: 186, mobile: 80 },
-	{ month: "February", desktop: 305, mobile: 200 },
-	{ month: "March", desktop: 237, mobile: 120 },
-	{ month: "April", desktop: 73, mobile: 190 },
-	{ month: "May", desktop: 209, mobile: 130 },
-	{ month: "June", desktop: 214, mobile: 140 },
-];
+import { getProfile } from "@/lib/actions/profile.action";
 
 function getTodayDate() {
 	const today = new Date();
@@ -100,17 +30,14 @@ function formatNumber(number: number) {
 	return formatted;
 }
 
-const Dashboard = ({ userData }: any) => {
+const Dashboard = () => {
 	const [todayExpenses, setTodayExpenses] = useState<number>(0);
 	const [todayIncomes, setTodayIncomes] = useState<number>(0);
 	const [transactions, setTransactions] = useState<Transaction[]>([]);
 	const [thisMonthTotalExpense, setThisMonthTotalExpense] =
 		useState<ThisMonthTotalExpense | null>(null);
 	const date = getTodayDate();
-
-	const totalVisitors = React.useMemo(() => {
-		return pieChartData.reduce((acc, curr) => acc + curr.visitors, 0);
-	}, []);
+	const [userData, setUserData] = useState<any>(null);
 
 	useEffect(() => {
 		const fetchUserData = async () => {
@@ -118,10 +45,12 @@ const Dashboard = ({ userData }: any) => {
 				const data: any = await getTodayTotalExpenses(date);
 				const latestTransaction = await getLatestTransactions();
 				const thisMonthTotalExpenseData = await getThisMonthTotalExpense();
+				const userData = await getProfile();
 
 				let expense = parseFloat(data.expense);
 				let income = parseFloat(data.income);
 
+				setUserData(userData.data);
 				setTodayExpenses(Number(expense));
 				setTodayIncomes(Number(income));
 				setTransactions(latestTransaction as Transaction[]);
@@ -234,96 +163,6 @@ const Dashboard = ({ userData }: any) => {
 					</div>
 				</div>
 			</div>
-			<div className="sm:md:lg:flex mt-8 gap-8 overflow-hidden">
-				{/* <div className="w-full shadow-sm p-4 rounded-xl mb-4 bg-white">
-					<h1 className="font-bold">Category Spending</h1>
-					<ChartContainer
-						config={pieChartConfig}
-						className="mx-auto aspect-square max-w-[400px]">
-						<PieChart>
-							<ChartTooltip
-								cursor={false}
-								content={<ChartTooltipContent hideLabel />}
-							/>
-							<Pie
-								data={pieChartData}
-								dataKey="visitors"
-								nameKey="browser"
-								innerRadius={60}
-								strokeWidth={10}>
-								<Label
-									content={({ viewBox }) => {
-										if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-											return (
-												<text
-													x={viewBox.cx}
-													y={viewBox.cy}
-													textAnchor="middle"
-													dominantBaseline="middle">
-													<tspan
-														x={viewBox.cx}
-														y={viewBox.cy}
-														className="fill-foreground text-3xl font-bold">
-														{totalVisitors.toLocaleString()}
-													</tspan>
-													<tspan
-														x={viewBox.cx}
-														y={(viewBox.cy || 0) + 24}
-														className="fill-muted-foreground">
-														Total Spent
-													</tspan>
-												</text>
-											);
-										}
-									}}
-								/>
-							</Pie>
-						</PieChart>
-					</ChartContainer>
-				</div> */}
-				{/* <div className="w-full shadow-sm rounded-xl p-4 mb-4 bg-white">
-					<div className="font-bold">6-Month Spending Statistic</div>
-					<ChartContainer config={chartConfig} className="mx-auto my-auto">
-						<AreaChart
-							accessibilityLayer
-							data={chartData}
-							margin={{
-								left: 12,
-								right: 12,
-							}}>
-							<CartesianGrid vertical={true} />
-							<XAxis
-								dataKey="month"
-								tickLine={false}
-								axisLine={false}
-								tickMargin={8}
-								tickFormatter={(value) => value.slice(0, 3)}
-							/>
-							<ChartTooltip
-								cursor={false}
-								content={<ChartTooltipContent indicator="dot" />}
-							/>
-							<Area
-								dataKey="mobile"
-								type="natural"
-								fill="var(--color-mobile)"
-								fillOpacity={0.4}
-								stroke="var(--color-mobile)"
-								stackId="a"
-							/>
-							<Area
-								dataKey="desktop"
-								type="natural"
-								fill="var(--color-desktop)"
-								fillOpacity={0.4}
-								stroke="var(--color-desktop)"
-								stackId="a"
-							/>
-						</AreaChart>
-					</ChartContainer>
-				</div> */}
-			</div>
-			{/* <div>Recent Transactions up to 10 transaction only.</div> */}
 		</section>
 	);
 };
